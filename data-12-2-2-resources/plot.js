db67d7ba5948f30904045b13878184bf49bc96af
John Beauchamp <94148420+1on1pt@users.noreply.github.com>
2022-03-10 07:19:10 -0600

    Updating files.

---
+++ data-12-2-2-resources/plot.js
@@ -0,0 +1,18 @@
+//console.log(cityGrowths);
+
+var sortedCities = cityGrowths.sort((a,b) => a.Increase_from_2016 - b.Increase_from_2016).reverse();
+var topFiveCityNames = cityGrowths.map(city => city.City).slice(0,7);
+var topFiveCityGrowths = cityGrowths.map(city => parseInt(city.Increase_from_2016)).slice(0,7);
+var trace = {
+    x: topFiveCityNames,
+    y: topFiveCityGrowths,
+    type: "bar"
+  };
+  var data = [trace];
+  var layout = {
+    title: "Most Rapidly Growing Cities",
+    xaxis: {title: "City" },
+    yaxis: {title: "Population Growth, 2016-2017"}
+  };
+  Plotly.newPlot("bar-plot", data, layout);
+console.log(topFiveCityGrowths.slice(0,7))
