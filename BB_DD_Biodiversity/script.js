4da55f0df6257f574feab42321dbbaf0b3931364
John Beauchamp <94148420+1on1pt@users.noreply.github.com>
2022-03-12 16:14:31 -0600

    Updating script.js file.

--- BB_DD_Biodiversity/script.js
+++ BB_DD_Biodiversity/script.js
@@ -2,7 +2,7 @@ d3.selectAll("body").on("change", updatePage);
 
 function updatePage() {
   var dropdownMenu = d3.selectAll("#selectOption").node();
-  var dropdownMenu2 = d3.selectAll("#selectOption");
+  // var dropdownMenu2 = d3.selectAll("#selectOption");
   var dropdownMenuID = dropdownMenu.id;
   var selectedOption = dropdownMenu.value;
 
