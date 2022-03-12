ab03f62f2024b4e15336d3e26783450eb432cf87
John Beauchamp <94148420+1on1pt@users.noreply.github.com>
2022-03-12 13:53:05 -0600

    Adding BB_DD_Biodiversity folder.

---
+++ BB_DD_Biodiversity/script.js
@@ -0,0 +1,15 @@
+d3.selectAll("body").on("change", updatePage);
+
+function updatePage() {
+  var dropdownMenu = d3.selectAll("#selectOption").node();
+  var dropdownMenu2 = d3.selectAll("#selectOption");
+  var dropdownMenuID = dropdownMenu.id;
+  var selectedOption = dropdownMenu.value;
+
+  console.log(dropdownMenuID);
+  console.log(selectedOption);
+  console.log(d3.selectAll("#menu").node().id);
+    // console.log(dropdownMenu);
+    // console.log(dropdownMenu2);
+
+};
